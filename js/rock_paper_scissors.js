75484df5fec43e2a038908b682196f484c9cadab
Viktor Vuger <vugerviktor22@gmail.com>
2023-07-18 15:26:33 +0200

    tried to make rock paper ...

---
+++ js/rock_paper_scissors.js
@@ -0,0 +1,21 @@
+//give value to elements
+const rock = "1";
+const paper = "2";
+const scissors = "3";
+
+rock < paper;
+paper < scissors;
+scissors < rock;
+
+//user gives their choice
+const userSelection = prompt("choose",'');
+console.log(userSelection);
+
+//computer pick random 
+const computerSelection = function getComputerChoice () {
+    
+}
+//computer sends result 
+
+
+
