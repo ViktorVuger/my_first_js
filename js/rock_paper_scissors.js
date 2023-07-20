409bc9e6a3b5baa8f095649541c653e5d51b3700
Viktor Vuger <vugerviktor22@gmail.com>
2023-07-20 15:30:37 +0200

    made get computer choice

--- js/rock_paper_scissors.js
+++ js/rock_paper_scissors.js
@@ -1,21 +1,36 @@
 //give value to elements
-const rock = "1";
-const paper = "2";
-const scissors = "3";
+//const rock = "1";
+//const paper = "2";
+//const scissors = "3";
 
-rock < paper;
-paper < scissors;
-scissors < rock;
+//rock < paper;
+//paper < scissors;
+//scissors < rock;
+//========================
+
 
 //user gives their choice
 const userSelection = prompt("choose",'');
 console.log(userSelection);
 
-//computer pick random 
-const computerSelection = function getComputerChoice () {
-    
-}
+
+// ================ get computer choice ================ 
+const getComputerChoice = () => {
+    let randomNumber = Math.floor(Math.random()*3);//Random number is for 3 choices between 0 and 2
+    switch(randomNumber){
+    case 0:
+      return'rock';
+    case 1:
+      return'paper';
+    case 2:
+      return'scissors';
+  }
+  }
+  
+// =============for running code=============
+  console.log(getComputerChoice());
+  console.log(getComputerChoice());
+  console.log(getComputerChoice());
+
 //computer sends result 
-
-
 
