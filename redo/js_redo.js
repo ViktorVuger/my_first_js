0afee626e0c56be34177d2dd9781e93b43c7591b
Viktor Vuger <vugerviktor22@gmail.com>
2023-08-17 10:35:34 +0200

    revision

--- redo/js_redo.js
+++ redo/js_redo.js
@@ -2,3 +2,37 @@
 let user='Viktor', age='17',message='Hello World';
 
 console.log(user,age,message);
+
+let userName;
+userName = 'John';
+
+let admin;
+admin = userName;
+alert(admin);
+
+
+
+const lotsOfDecimal = 1.766584958675746364;
+lotsOfDecimal;
+const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
+twoDecimalPlaces;
+
+console.log(twoDecimalPlaces);
+
+
+let a = 10;
+console.log(a);
+console.log(a*9);
+
+let b = 7 * a;
+console.log(b);
+
+let max = 57; actual = max - 13;
+let percentage = actual/max;
+
+// ======= FUNCTIONS ======= 
+const myText = 'I am string';
+const myNewText = myText.replace('string','sausage');
+
+console.log(myText);
+console.log(myNewText);
