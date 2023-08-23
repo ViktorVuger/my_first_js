e30f91e66164f7c736d38908ff75b108c2fe4017
Viktor Vuger <vugerviktor22@gmail.com>
2023-08-23 15:51:06 +0200

    started css rps

--- js/rock_paper_scissors.js
+++ js/rock_paper_scissors.js
@@ -1,54 +1,67 @@
 
 //user gives their choice
-let userChoice = prompt("choose",'');
-console.log(userChoice);
+//let userChoice = prompt("choose",'');
+//console.log(userChoice);
 
-const player = () => {
-  switch(userChoice){
-    case 'rock':
-      return 1;
-    case 'paper':
-      return 2;
-    case 'scissors':
-      return 3;
+//=============================================================
+document.addEventListener('DOMContentLoaded', () => {
+  let player = 0;
+  const buttonRock = document.querySelector('#button_rock')
+  const buttonPaper = document.querySelector('#button_paper')
+  const buttonScissors = document.querySelector('#button_scissors')
+
+  buttonRock.addEventListener('click', () => {
+    player = 1;
+  });
+
+  buttonPaper.addEventListener('click', () => {
+    player = 2;
+  });
+
+  buttonScissors.addEventListener('click', () => {
+    player = 3;
+  });
+
+  const playerChoice = player;
+  console.log(playerChoice);
+
+  console.log(playerChoice)
+  // ================ get computer choice ================ 
+  const getComputerChoice =  Math.floor(Math.random()*3)+1;//Random number is for 3 choices between 0 and 2
+
+
+
+  const computerChoice = getComputerChoice;
+    
+  // =============for running code=============
+    console.log(computerChoice );
+    //console.log(getComputerChoice());
+    //console.log(getComputerChoice());
+
+  // ========== mapping choices ==========
+
+
+    //============== compare choices ==============
+
+
+  //console.log(determineWinner());
+    //computer sends result 
+
+  // const choice1 = 1;
+  // const choice2 = 1;
+
+  const getWinner = (choice1, choice2) => {
+    if( choice1 - choice2  > 0 ) {
+      return('player wins!');
+    }else if (choice1 - choice2 === 0){
+      return('its a tie');
+    }else {
+      return('computer wins!');
+    }
   }
-}
-console.log(player());
-
-// ================ get computer choice ================ 
-const getComputerChoice =  Math.floor(Math.random()*3)+1;//Random number is for 3 choices between 0 and 2
 
 
+  const winner = getWinner(playerChoice, computerChoice);
+  console.log(winner);
 
-const computerChoice = getComputerChoice;
-  
-// =============for running code=============
-  console.log(computerChoice );
-  //console.log(getComputerChoice());
-  //console.log(getComputerChoice());
-
-// ========== mapping choices ==========
-
-
-  //============== compare choices ==============
-
-
-//console.log(determineWinner());
-  //computer sends result 
-
-// const choice1 = 1;
-// const choice2 = 1;
-
-const getWinner = (choice1, choice2) => {
-  if( choice1 - choice2  > 0 ) {
-    return('player wins!');
-  }else if (choice1 - choice2 === 0){
-    return('its a tie');
-  }else {
-    return('computer wins!');
-  }
-}
-
-
-const winner = getWinner(player(), computerChoice);
-console.log(winner);
+});
